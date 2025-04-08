ce595d0a8134cf8b6342df93843146a191df95b8
Nicholas Mirchandani <nmirchandani@chapman.edu>
2025-04-07 22:13:33 -0700

    Rest of the color unit tests

--- tests/color_test.rs
+++ tests/color_test.rs
@@ -4,6 +4,7 @@ mod test_hack;
 
 #[cfg(test)]
 mod color_test {
+
     // Import the necessary modules.
     use crate::color::Color;
     use crate::test_hack::expect_eq;
@@ -48,6 +49,77 @@ mod color_test {
                 blue: 1.0
             }
         );
+        unsafe { teardown() };
+    }
+
+    // Ensures colors can be subtracted.
+    #[test]
+    fn test_subtract_color() {
+        unsafe { setup() };
+        let c1 = Color {
+            red: 0.9,
+            green: 0.6,
+            blue: 0.75,
+        };
+        let c2 = Color {
+            red: 0.7,
+            green: 0.1,
+            blue: 0.25,
+        };
+        expect_eq!(
+            c1 - c2,
+            Color {
+                red: 0.2,
+                green: 0.5,
+                blue: 0.5
+            }
+        );
+        unsafe { teardown() };
+    }
+
+    // Ensures scalar multiplication works as intended.
+    #[test]
+    fn test_scalar_multiply() {
+        unsafe { setup() };
+        let c = Color {
+            red: 0.2,
+            green: 0.3,
+            blue: 0.4,
+        };
+        expect_eq!(
+            c * 2.0,
+            Color {
+                red: 0.4,
+                green: 0.6,
+                blue: 0.8
+            }
+        );
+        unsafe { teardown() };
+    }
+
+    // Ensures color multiplication works as intended.
+    #[test]
+    fn test_color_multiply() {
+        unsafe { setup() };
+        let c1 = Color {
+            red: 1.0,
+            green: 0.2,
+            blue: 0.4,
+        };
+        let c2 = Color {
+            red: 0.9,
+            green: 1.0,
+            blue: 0.1,
+        };
+        expect_eq!(
+            c1 * c2,
+            Color {
+                red: 0.9,
+                green: 0.2,
+                blue: 0.04
+            }
+        );
+
         unsafe { teardown() };
     }
 }
