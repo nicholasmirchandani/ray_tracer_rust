c2dfdd812716c22c490f0bce26542dfaa4cd0a3c
Nicholas Mirchandani <nmirchandani@chapman.edu>
2025-04-12 20:45:22 -0700

    Canvas Test

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,10 +1,19 @@
+#[path = "./color.rs"] 
+mod color;
+use crate::color::Color;
+
 pub struct Canvas {
-    pub width : u32;
-    pub height : u32;
+    pub width : usize,
+    pub height : usize,
+
+    // Note 2D array is inefficient, but logically most simple.
+    pub pixels : Vec<Vec<Color>>,
 }
 
 impl Canvas {
-    pub fn new(width: u32, height: u32) -> Self {
-        Canvas { width, height }
+    pub fn new(width: usize, height: usize) -> Self {
+        // Refer to pixel as pixel[width, height]
+        let pixels : Vec<Vec<Color>> = vec![vec![Color::new(0.0, 0.0, 0.0); height]; width];
+        Canvas { width, height, pixels }
     }
 }
---
+++ tests/canvas_test.rs
@@ -0,0 +1,32 @@
+#[path = "../src/canvas.rs"]
+mod canvas;
+#[path = "../src/color.rs"]
+mod color;
+mod test_hack;
+
+#[cfg(test)]
+mod canvas_test {
+    // Import the necessary modules.
+    use crate::canvas::Canvas;
+    use crate::color::Color;
+    use crate::test_hack::expect_eq;
+    use crate::test_hack::expect_float_eq;
+    use crate::test_hack::setup;
+    use crate::test_hack::teardown;
+
+    // Ensures canvases can be properly created.
+    #[test]
+    fn test_create_canvas() {
+        unsafe { setup() };
+        let c = Canvas::new(10, 20);
+        expect_eq!(c.width, 10);
+        expect_eq!(c.height, 20);
+        
+        for column in c.pixels.iter() {
+            for pixel in column.iter() {
+                expect_eq!(*pixel, Color::new(0.0,0.0,0.0));
+            }
+        }
+        unsafe { teardown() };
+    }
+}
